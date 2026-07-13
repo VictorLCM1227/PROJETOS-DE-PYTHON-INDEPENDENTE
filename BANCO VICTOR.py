eeca4b41b7e3bafb6ccfd9fede9aa9b3ea65f63c
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-07-12 23:41:13 -0300

    Create BANCO VICTOR.py

---
+++ BANCO VICTOR.py
@@ -0,0 +1,18 @@
+
+
+print('''[0] Para sair
+[1] Mostrar o extrato
+[2] Consultar saldo
+[3] Depositar dinheiro
+[4] Sacar dinheiro ''')
+opcao = int(input('O que deseja fazer? '))
+
+if opcao == 0:
+
+elif opcao == 1:
+
+elif opcao == 2:
+
+elif opcao == 3:
+
+elif opcao == 4:
