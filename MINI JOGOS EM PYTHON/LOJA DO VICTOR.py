ea7e6a90de978468ec2c9643ab22d21af1a10dc3
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-07-20 21:15:34 -0300

    Update LOJA DO VICTOR.py

--- MINI JOGOS EM PYTHON/LOJA DO VICTOR.py
+++ MINI JOGOS EM PYTHON/LOJA DO VICTOR.py
@@ -11,6 +11,34 @@ produtos = [
 
 carrinho = []
 
+def ver_produtos():
+    print('Produtos: ')
+    for codigo, produto in enumerate(produtos):
+        print(f'Código: {codigo} - Produto: {produto["nome"]:.<15}  R${produto["preco"]:6>}')
+    print('-' * 30)
+
+def comprar_produtos():
+    while True:
+        while True:
+            interesse = int(input('Código do produto que deseja comprar: '))
+            if 0 <= interesse < len(produtos):
+                carrinho.append(produtos[interesse])
+                break
+            print('Código inválido.')
+        continuar = input('Deseja adicionar mais produtos ao carrinho? [S/N]').strip().upper()[0]
+        if continuar == 'N':
+            break
+    print('-' * 30)
+
+def ver_carrinho():
+    print('CARRINHO: ')
+    if not carrinho:
+        print('O carrinho está vazio.')
+    else:
+        for produto in carrinho:
+            print(f'Produto: {produto["nome"]} - R${produto["preco"]}')
+
+
 while True:
     print('''[0] Sair
 [1] Ver produtos
@@ -31,33 +59,15 @@ while True:
 
 #VER PRODUTOS
     elif escolha == 1:
-        print('Produtos: ')
-        for codigo, produto in enumerate(produtos):
-            print(f'Código: {codigo} - Produto: {produto["nome"]:.<15}  R${produto["preco"]:6>}')
-        print('-' * 30)
+        ver_produtos()
 
 #COMPRAR PRODUTOS
     elif escolha == 2:
-        while True:
-            while True:
-                interesse = int(input('Código do produto que deseja comprar: '))
-                if 0 <= interesse < len(produtos):
-                    carrinho.append(produtos[interesse])
-                    break
-                print('Código inválido.')
-            continuar = input('Deseja adicionar mais produtos ao carrinho? [S/N]').strip().upper()[0]
-            if continuar == 'N':
-                break
-        print('-' * 30)
+        comprar_produtos()
 
 #VER CARRINHO
     elif escolha == 3:
-        print('CARRINHO: ')
-        if not carrinho:
-            print('O carrinho está vazio.')
-        else:
-            for produto in carrinho:
-                print(f'Produto: {produto["nome"]} - R${produto["preco"]}')
+        ver_carrinho()
 
 #FINALIZAR COMPRA
     elif escolha == 4:
