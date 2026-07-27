b6b6a1e87307f9c5a01a6d9ff94921436f8f599f
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-07-27 16:02:21 -0300

    Update CALCULADORA FISICA.py
    v1 concluida

--- COMPUTAÇÃO CIENTIFICA/CALCULADORA FISICA.py
+++ COMPUTAÇÃO CIENTIFICA/CALCULADORA FISICA.py
@@ -1,8 +1,10 @@
+from math import sqrt
+
 def leiaInt(msg):
     while True:
         try:
             numero = int(input(msg))
-        except (ValueError, TypeError):
+        except ValueError:
             print('\033[31mERRO: por favor, digite um número interiro válido.\033[m')
             continue
         except KeyboardInterrupt:
@@ -15,7 +17,7 @@ def leiaFloat(msg):
     while True:
         try:
             numero = float(input(msg))
-        except (ValueError, TypeError):
+        except ValueError:
             print('\033[31mERRO: por favor, digite um número real válido.\033[m')
             continue
         except KeyboardInterrupt:
@@ -28,7 +30,7 @@ def leiaFloatPositivo(msg):
     while True:
         try:
             numero = float(input(msg))    
-        except (ValueError, TypeError):
+        except ValueError:
             print('\033[31mERRO: por favor, digite um número real válido.\033[m')
             continue
         except KeyboardInterrupt:
@@ -58,44 +60,44 @@ def menu(lista, menu_msg='MENU PRINCIPAL'):
     return opcao
 
 def calcular_velocidade():
-    distancia_percorrida = leiaFloatPositivo('Distância percorrida em Km: ')
-    tempo_gasto = leiaFloatPositivo('Tempo gasto em horas: ')
+    distancia_percorrida = leiaFloatPositivo('Distância percorrida em m: ')
+    tempo_gasto = leiaFloatPositivo('Tempo gasto em segundos: ')
     velocidade = distancia_percorrida / tempo_gasto
     print(linha())
     print('Fórmula:')
     print('v = Δs / Δt')
     print('Substituindo:')
-    print(f'v = {distancia_percorrida} km / {tempo_gasto} h')
+    print(f'v = {distancia_percorrida} m / {tempo_gasto} s')
     print('Resultado:')
-    print(f'v = {velocidade:.2f} km/h')
+    print(f'v = {velocidade:.2f} m/s')
     print(linha())
     input('Pressione ENTER para continuar...')
 
 def calcular_distancia():
-    velocidade = leiaFloatPositivo('Velocidade em Km/h: ')
-    tempo_gasto = leiaFloatPositivo('Tempo gasto em horas: ')
+    velocidade = leiaFloatPositivo('Velocidade em m/s: ')
+    tempo_gasto = leiaFloatPositivo('Tempo gasto em segundos: ')
     distancia_percorrida = velocidade * tempo_gasto
     print(linha())
     print('Fórmula:')
     print('Δs = v × t')
     print('Substituindo:')
-    print(f'Δs = {velocidade} km/h × {tempo_gasto} h')
+    print(f'Δs = {velocidade} m/s × {tempo_gasto} s')
     print('Resultado:')
-    print(f'Δs = {distancia_percorrida:.2f} km')
+    print(f'Δs = {distancia_percorrida:.2f} m')
     print(linha())
     input('Pressione ENTER para continuar...')
 
 def calcular_tempo():
-    distancia_percorrida = leiaFloatPositivo('Distância percorrida em Km: ')
-    velocidade = leiaFloatPositivo('Velocidade em Km/h: ')
+    distancia_percorrida = leiaFloatPositivo('Distância percorrida em m: ')
+    velocidade = leiaFloatPositivo('Velocidade em m/s: ')
     tempo_gasto = distancia_percorrida / velocidade
     print(linha())
     print('Fórmula:')
     print('Δt = Δs / v')
     print('Substituindo:')
-    print(f'Δt = {distancia_percorrida} km / {velocidade} km/h')
+    print(f'Δt = {distancia_percorrida} m / {velocidade} m/s')
     print('Resultado:')
-    print(f'Δt = {tempo_gasto:.2f} h')
+    print(f'Δt = {tempo_gasto:.2f} s')
     print(linha())
     input('Pressione ENTER para continuar...')
 
@@ -134,24 +136,91 @@ def calcular_tempo_mruv():
     velocidade_final = leiaFloat('Velocidade final em m/s: ')
     velocidade_inicial = leiaFloat('Velocidade inicial em m/s: ')
     aceleracao = leiaFloat('Aceleração em m/s²: ')
-    tempo = (velocidade_final - velocidade_final) / aceleracao
+    try:
+        tempo = (velocidade_final - velocidade_inicial) / aceleracao
+    except ZeroDivisionError:
+        print('Não foi possível dividir por 0.')
+    else:
+        print(linha())
+        print('Fórmula:')
+        print('t = (v - v0) / a')
+        print('Substituindo:')
+        print(f't = ({velocidade_final} - {velocidade_inicial}) / {aceleracao}')
+        print('Resultado:')
+        print(f't = {tempo:.2f} s')
+        print(linha())
+        input('Pressione ENTER para continuar...')
+
+def calcular_deslocamento():
+    velocidade = leiaFloat('Velocidade inicial em m/s: ')
+    tempo = leiaFloatPositivo('Tempo gasto em segundos: ')
+    aceleracao = leiaFloat('Aceleração em m/s²: ')
+    deslocamento = velocidade * tempo + ((aceleracao * tempo**2) / 2)
     print(linha())
     print('Fórmula:')
-    print('t = (v - v0) / a')
+    print('s = v0t + (at²)/2')
     print('Substituindo:')
-    print()
+    print(f's = {velocidade} x {tempo} + ({aceleracao} x {tempo**2})/2')
     print('Resultado:')
-    print(f'a = {aceleracao:.2f} m/s²')
+    print(f's = {deslocamento:.2f} m')
     print(linha())
     input('Pressione ENTER para continuar...')
 
+def calcular_torricelli():
+    while True:
+        opcao = menu(['Voltar', 'Velocidade final', 'Deslocamento'], menu_msg='TORRICELLI')
+        if opcao == 0:
+            cabecalho('Voltar')
+            break
+        elif opcao == 1:
+            cabecalho('velocidade final')
+            velocidade_inicial = leiaFloat('Velocidade inicial em m/s: ')
+            aceleracao = leiaFloat('Aceleração em m/s²: ')
+            distancia_percorrida = leiaFloatPositivo('Distância percorrida em m: ')
+            try:
+                velocidade_final = sqrt(velocidade_inicial**2 + 2 * aceleracao * distancia_percorrida)
+            except ValueError:
+                print('A resposta não existe no conjunto dos números reais.')
+            else:
+                print(linha())
+                print('Fórmula:')
+                print('v = √(v0² + 2*a*Δs)')
+                print('Substituindo:')
+                print(f'v = √({velocidade_inicial}² + 2 * {aceleracao} * {distancia_percorrida})')
+                print('Resultado:')
+                print(f'v = {velocidade_final:.2f} m/s')
+                print(linha())
+                input('Pressione ENTER para continuar...')
+
+        elif opcao == 2:
+            cabecalho('deslocamento')
+            velocidade_inicial = leiaFloat('Velocidade inicial em m/s: ')
+            velocidade_final = leiaFloat('Velocidade final em m/s: ')
+            aceleracao = leiaFloat('Aceleração em m/s²: ')
+            try:
+                deslocamento = (velocidade_final**2 - velocidade_inicial**2) / (2 * aceleracao)
+            except ZeroDivisionError:
+                print('Não é possível calcular deslocamento com aceleração igual a zero.')
+            else:
+                print(linha())
+                print('Fórmula:')
+                print('Δs = (v² - v0²) / (2 x a)')
+                print('Substituindo:')
+                print(f'Δs = ({velocidade_final}² - {velocidade_inicial}²) / (2 x {aceleracao})')
+                print('Resultado:')
+                print(f's = {deslocamento:.2f} m')
+                print(linha())
+                input('Pressione ENTER para continuar...')
+
+        else:
+            print('Opção inválida.')
 
 while True:
     cabecalho('CALCULADORA DE FÍSICA', simbolo='=')
     opcao_menu = menu(['SAIR', 'MRU', 'MRUV'])
 
     if opcao_menu == 0:
-        cabecalho('SAIR')
+        cabecalho('SAINDO')
         break
 
     elif opcao_menu == 1:
@@ -183,14 +252,17 @@ while True:
                 cabecalho('Velocidade final')
                 calcular_velocidade_final()
             elif opcao_submenu_mruv == 2:
-                cabecalho('Aceleração')
+                cabecalho('Aceleração') 
                 calcular_aceleracao()
             elif opcao_submenu_mruv == 3:
                 cabecalho('Tempo')
+                calcular_tempo_mruv()
             elif opcao_submenu_mruv == 4:
                 cabecalho('Deslocamento')
+                calcular_deslocamento()
             elif opcao_submenu_mruv == 5:
                 cabecalho('Torricelli')
+                calcular_torricelli()
             else:
                 print('Opção inválida.')
 
