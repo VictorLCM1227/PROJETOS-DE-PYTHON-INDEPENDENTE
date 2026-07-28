323cdae2f1a4cc0e4dc057af5f47907135e93cdd
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-07-27 21:14:19 -0300

    Update ANALISADOR DE GASTOS PESSOAIS.py
    v1 finalizado

--- FinTech/ANALISADOR DE GASTOS PESSOAIS.py
+++ FinTech/ANALISADOR DE GASTOS PESSOAIS.py
@@ -40,7 +40,7 @@ def leiaFloatPositivo(msg):
                 return numero
             print('\033[31mERRO: por favor, digite um número real válido e maior do que zero.\033[m')
 
-def linha(simbolo='-', tam = 42):
+def linha(simbolo='-', tam=42):
     return simbolo * tam 
 
 def cabecalho(txt, simbolo='-'):
@@ -109,10 +109,43 @@ def media_dos_gastos():
         print(f'A média dos gastos é R${media:.2f}')
 
 def maior_gasto():
+    if not gastos:
+        print('Ainda não há gastos registrados.')
+    else:
+        maior = gastos[0]
+        for gasto in gastos:
+            if gasto['valor'] > maior['valor']:
+                maior = gasto
+        print(f'{"ID":<4}{"Descrição":<20} {"Valor":<12} {"Categoria":<12}')
+        print(f"{maior['id']:<4} {maior['descricao']:<20} {maior['valor']:<12.2f} {maior['categoria']:<12}")
+
+def menor_gasto():
+    if not gastos:
+        print('Ainda não há gastos registrados.')
+    else:
+        menor = gastos[0]
+        for gasto in gastos:
+            if gasto['valor'] < menor['valor']:
+                menor = gasto
+        print(f'{"ID":<4}{"Descrição":<20} {"Valor":<12} {"Categoria":<12}')
+        print(f"{menor['id']:<4} {menor['descricao']:<20} {menor['valor']:<12.2f} {menor['categoria']:<12}")
+
+def gastos_por_categoria():
+    if not gastos:
+        print('Ainda não há gastos registrados.')
+    else:
+        print(f'{"Categoria":<15} {"Total":<15}')
+        print(linha())
+        for categoria_atual in categorias:
+            total = 0
+            for gasto in gastos:
+                if gasto['categoria'] == categoria_atual:
+                    total += gasto['valor']
+            print(f'{categoria_atual:<15} : R${total:<15.2f}')
 
 while True:
     opcao = menu(['Sair', 'Adicionar gasto', 'Listar gastos', 'Ver total gasto',
-                   'Ver gastos por categoria', 'Maior gasto', 'Média dos gastos'], menu_msg='CONTROLE FINANCEIRO')
+                   'Ver gastos por categoria', 'Maior gasto', 'Média dos gastos', 'Menor gasto'], menu_msg='CONTROLE FINANCEIRO')
 
     if opcao == 0:
         cabecalho('Saindo')
@@ -131,12 +164,20 @@ while True:
 
     elif opcao == 4:
         cabecalho('Ver gastos por categoria')
+        gastos_por_categoria()
 
     elif opcao == 5:
         cabecalho('Maior gasto')
+        maior_gasto()
 
     elif opcao == 6:
         cabecalho('Média dos gastos')
         media_dos_gastos()
+
+    elif opcao == 7:
+        cabecalho('Menor gasto')
+        menor_gasto()
+
     else:
         print('Opção inválida.')
+        
