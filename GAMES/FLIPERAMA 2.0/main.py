bd776249dec86cef3ff27651b17c640a94cccccc
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-01 09:15:32 -0300

    updates

---
+++ GAMES/FLIPERAMA 2.0/main.py
@@ -0,0 +1,47 @@
+
+def leiaInt(msg):
+    while True:
+        try:
+            numero = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número interiro válido.\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return numero
+
+def linha(tam = 42):
+    return '-' * tam 
+
+def cabeçalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+def menu(lista, menu_titulo='MENU PRINCIPAL'):
+    cabeçalho(menu_titulo)
+    contador = 0
+    for item in lista:
+        print(f'\033[33m[{contador}]\033[m - \033[34m{item}\033[m')
+        contador += 1
+    print(linha())
+    opcao = leiaInt('\033[32mSua opção: \033[m')
+    return opcao
+
+#menu
+while True:
+    escolha_principal = menu(lista=['SAIR', 'JOGAR', 'PERFIL', 'CARTEIRA'], menu_titulo='FLIPERAMA DO VICTOR')
+
+    if escolha_principal == 0:
+        cabeçalho('SAINDO')
+        break
+
+    elif escolha_principal == 1:
+        while True:
+            escolha_jogo = menu(lista=['VOLTAR', 'JOKENPÔ', 'PAR OU ÍMPAR', 'ADVINHE O NÚMERO'], menu_titulo='JOGOS')
+
+            if escolha_jogo == 0:
+                cabeçalho('VOLTANDO')
+                break
