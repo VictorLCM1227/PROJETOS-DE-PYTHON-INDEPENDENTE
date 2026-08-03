1db489a887ff573128769397698544e18e05ebc2
Victor Luan Costa Matos <victor.luan.costa.matos@gmail.com>
2026-08-03 15:29:35 -0300

    updates

--- GAMES/FLIPERAMA 2.0/jogador.py
+++ GAMES/FLIPERAMA 2.0/jogador.py
@@ -1,14 +1,105 @@
 #jogador
 ficha_do_jogador = {
     'nome': input('Qual o seu nome? ').strip(),
-    'saldo': 10,
-    'partidas':  0,
-    'vitorias': 0,
-    'derrotas': 0,
-    'empates': 0,
-    'maior_sequencia': 0,
-    'dinheiro_ganho': 0,
-    'dinheiro_perdido':0,
-    #armazenar os dados do extrato em uma tupla
-    'extrato': []
+    
+    'partidas_totais':  0,
+    'vitorias_totais': 0,
+    'derrotas_totais': 0,
+    'empates_totais': 0,
+    'dinheiro_ganho_total': 0,
+    'dinheiro_perdido_total':0,
+    
+    'conquistas': {
+        'primeira_vitoria': False,
+        '10_vitorias': False,
+        '50_vitorias': False,
+        '100_vitorias': False,
+        'milionario': False
+    },
+    'carteira': {
+        'extrato': [],
+        'saldo': 10,
+        'maior_aposta_feita' :0,
+    },
+
+    
+
+    'estatisticas_jogos': {
+        'jokenpo': {
+            'partidas': 0,
+            'vitorias': 0,
+            'derrotas': 0,
+            'empates': 0,
+            'sequencia_atual': 0,
+            'melhor_sequencia':0,
+            'dinheiro_ganho': 0,
+            'dinheiro_perdido':0,
+            'maior_aposta':0
+        }
+    ,
+    'par_ou_impar': {
+            'partidas': 0,
+            'vitorias': 0,
+            'derrotas': 0,
+            'empates': 0,
+            'sequencia_atual': 0,
+            'melhor_sequencia':0,
+            'dinheiro_ganho': 0,
+            'dinheiro_perdido':0,
+            'maior_aposta':0
+        }
+    ,
+    'adivinhe_o_numero': {
+            'partidas': 0,
+            'vitorias': 0,
+            'derrotas': 0,
+            'empates': 0,
+            'sequencia_atual': 0,
+            'melhor_sequencia':0,
+            'dinheiro_ganho': 0,
+            'dinheiro_perdido':0,
+            'maior_aposta':0
+        },
+    'blackjack': {
+            'partidas': 0,
+            'vitorias': 0,
+            'derrotas': 0,
+            'empates': 0,
+            'sequencia_atual': 0,
+            'melhor_sequencia':0,
+            'dinheiro_ganho': 0,
+            'dinheiro_perdido':0,
+            'maior_aposta':0
+        },
+    'corrida_de_cavalos': {
+            'partidas': 0,
+            'vitorias': 0,
+            'derrotas': 0,
+            'empates': 0,
+            'sequencia_atual': 0,
+            'melhor_sequencia':0,
+            'dinheiro_ganho': 0,
+            'dinheiro_perdido':0,
+            'maior_aposta':0
+            
+        },
+    'dados': {
+            'partidas': 0,
+            'vitorias': 0,
+            'derrotas': 0,
+            'empates': 0,
+            'sequencia_atual': 0,
+            'melhor_sequencia':0,
+            'dinheiro_ganho': 0,
+            'dinheiro_perdido':0,
+            'maior_aposta':0
+        },
+    
+
+
+
+
+
+
+    }
 }
--- GAMES/FLIPERAMA 2.0/perfil.py
+++ GAMES/FLIPERAMA 2.0/perfil.py
@@ -6,3 +6,6 @@ def mostrar_perfil(ficha_do_jogador):
     #desempacotar e mostrar dicionario
     for atributo, valor in ficha_do_jogador.items():
         print(f'{atributo}: {valor}')
+    print(linha())
+    taxa_de_vitoria = ficha_do_jogador['vitoria'] / ficha_do_jogador['partidas']
+    print(f'Taxa de vitória: {taxa_de_vitoria:.2f}%')
